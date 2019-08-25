0f13b6d578c828cdef39d66db85a870e2afe597f
james <james@admins-iMac-3.local>
2019-08-25 10:58:58 -0500

    added bullets

--- src/app/main.js
+++ src/app/main.js
@@ -19,6 +19,7 @@ let world = {
             y:0,
         }
     },
+    gravityBoundObjects:[],
     level:[
         Sprite({
             x:0,
@@ -28,6 +29,15 @@ let world = {
             color:'rgba(51,156,128)'
         }),
     ],
+    bullets:[],
+    updateBullets:function(){
+        for(let bullet in this.bullets) {
+            this.bullets[bullet].sprite.h_speed -=0.1;
+            
+            this.bullets[bullet].sprite.x += this.bullets[bullet].sprite.h_speed;
+             
+        }
+    },
     updateGravity:function(entities){
         for(let gravObj in entities) {
 
@@ -55,12 +65,39 @@ let pawn = Sprite({
     y:0,
     width:40,
     height:50,
+    anchor:{
+        x:0.5,
+        y:0.5,
+    },
+
     // custom params
     v_speed:0,
     h_speed:0,
     weight:1,
     
 });
+
+world.gravityBoundObjects.push(pawn);
+
+class Bullet {
+    constructor(params){
+        this.sprite = Sprite({
+            width:5,
+            height:5,
+            color:'red',
+            x:params.x,
+            y:params.y,
+            dx:2,
+            ttl:10,
+            v_speed:0,
+            h_speed:10,
+            weight:0.1,
+        });
+    }
+    }
+
+
+
 //
 // Load Image Assets and assign to objects
 //
@@ -84,7 +121,7 @@ let loop = GameLoop({  // create the main game loop
     update: function() { // update the game state
       pawn.update();
       
-  
+
       // wrap the sprites position when it reaches
       // the edge of the screen
       if (pawn.x > canvas.width) {
@@ -97,27 +134,51 @@ let loop = GameLoop({  // create the main game loop
         if (keyPressed('up')){
             pawn.v_speed -=0.5;
         }
-        if (keyPressed('left')){
-            pawn.h_speed -=0.1;
-        }
         if (keyPressed('right')){
             pawn.h_speed +=0.1;
+            if(pawn.rotation <= 0.3) {
+                pawn.rotation +=0.05;
+            }
+        }
+        if (keyPressed('left')){
+            pawn.h_speed -=0.1;
+            if(pawn.rotation >= -0.3) {
+                pawn.rotation -=0.05;
+            }
+        }
+        if (keyPressed('space')){
+            let bullet = new Bullet({
+                x:pawn.x+15,
+                y:pawn.y+10
+            });
+            world.gravityBoundObjects.push(bullet.sprite);
+
+            world.bullets.push(bullet);
         }
 
-      let gravityBoundObjects = [pawn];
-
+     
 
       // move pawn horizontally
 
       pawn.x = pawn.x + pawn.h_speed;
+      if(pawn.rotation >0) {
+        pawn.rotation -=0.025;
+      }else if(pawn.rotation < 0) {
+        pawn.rotation +=0.025;
 
+      }
+      
+      world.updateBullets();
 
-      world.updateGravity(gravityBoundObjects);
+      world.updateGravity(world.gravityBoundObjects);
 
     },
     render: function() { // render the game state
       pawn.render();
-
+        
+      for(let ent in world.bullets) {
+            world.bullets[ent].sprite.render();
+        }
       for(let ent in world.level) {
           world.level[ent].render();
       }
